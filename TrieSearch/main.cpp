39a52b607724ef6bf148245cd3b6322b2111e1f0
Vinod Gupta <vinod_gupta@live.com>
2015-01-29 16:58:11 -0800

    initial check in

--- TrieSearch/main.cpp
+++ TrieSearch/main.cpp
@@ -8,6 +8,53 @@
 
 #include <iostream>
 
+class TrieNode
+{
+public:
+    TrieNode(char k,int v):_key(k),_val(v),_next(NULL),_children(NULL)
+    {}
+    virtual ~TrieNode()
+    {
+        if(_next)
+            delete _next;
+        if(_children)
+            delete _children;
+    }
+    
+    char key() {return _key;}
+    int val() {return _val;}
+    
+    TrieNode *next() {return _next;}
+    TrieNode *child() {return _children;}
+    
+    void AddNext(TrieNode *next) {_next = next;}
+    void AddChild(TrieNode *child) {_children = child;}
+    
+private:
+    char _key;
+    int _val;
+    TrieNode *_next;
+    TrieNode *_children;
+};
+
+class Trie
+{
+public:
+    Trie()
+    {
+        _root = new TrieNode('.',0);
+    }
+    virtual ~Trie()
+    {
+        if(_root)
+            delete _root;
+    }
+    
+    
+private:
+    TrieNode *_root;
+};
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
